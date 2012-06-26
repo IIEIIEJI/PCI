2d849fc0407692431a9d2a86e7fa0f4b061e9755
IIEIIEJI <tburnashevr@yandex.ru>
2012-06-27 02:33:47 +0400

    node

--- uploader/app.js
+++ uploader/app.js
@@ -24,67 +24,69 @@ function handler (req, res) {
 }
 
 io.sockets.on('connection', function (socket) {
-    //Events will go here
-});
-var Files = {};
-socket.on('Start', function (data) { //data contains the variables that we passed through in the html file
-    var Name = data['Name'];
-    Files[Name] = {  //Create a new Entry in The Files Variable
-        FileSize : data['Size'],
-        Data   : "",
-        Downloaded : 0
-    }
-    var Place = 0;
-    try{
-        var Stat = fs.statSync('Temp/' +  Name);
-        if(Stat.isFile())
-        {
-            Files[Name]['Downloaded'] = Stat.size;
-            Place = Stat.size / 524288;
+    socket.on('Start', function (data) { //data contains the variables that we passed through in the html file
+        var Name = data['Name'];
+        console.log(Name);
+        var Files ={};
+        Files[Name] = {  //Create a new Entry in The Files Variable
+            FileSize : data['Size'],
+            Data	 : "",
+            Downloaded : 0
         }
-    }
-    catch(er){} //It's a New File
-    fs.open("Temp/" + Name, "a", 0755, function(err, fd){
-        if(err)
+        var Place = 0;
+        try{
+            var Stat = fs.statSync('Temp/' +  Name);
+            if(Stat.isFile())
+            {
+                Files[Name]['Downloaded'] = Stat.size;
+                Place = Stat.size / 524288;
+            }
+        }
+        catch(er){} //It's a New File
+        fs.open("Temp/" + Name, 'a', 0755, function(err, fd){
+            if(err)
+            {
+                console.log(err);
+            }
+            else
+            {
+                Files[Name]['Handler'] = fd; //We store the file handler so we can write to it later
+                socket.emit('MoreData', { 'Place' : Place, Percent : 0 });
+            }
+        });
+    });
+
+    socket.on('Upload', function (data){
+        var Name = data['Name'];
+        Files[Name]['Downloaded'] += data['Data'].length;
+        Files[Name]['Data'] += data['Data'];
+        if(Files[Name]['Downloaded'] == Files[Name]['FileSize']) //If File is Fully Uploaded
         {
-            console.log(err);
+            fs.write(Files[Name]['Handler'], Files[Name]['Data'], null, 'Binary', function(err, Writen){
+                var inp = fs.createReadStream("Temp/" + Name);
+                var out = fs.createWriteStream("Video/" + Name);
+                util.pump(inp, out, function(){
+                    fs.unlink("Temp/" + Name, function () { //This Deletes The Temporary File
+                        exec("ffmpeg -i Video/" + Name  + " -ss 01:30 -r 1 -an -vframes 1 -f mjpeg Video/" + Name  + ".jpg", function(err){
+                            socket.emit('Done', {'Image' : 'Video/' + Name + '.jpg'});
+                        });
+                    });
+                });
+            });
+        }
+        else if(Files[Name]['Data'].length > 10485760){ //If the Data Buffer reaches 10MB
+            fs.write(Files[Name]['Handler'], Files[Name]['Data'], null, 'Binary', function(err, Writen){
+                Files[Name]['Data'] = ""; //Reset The Buffer
+                var Place = Files[Name]['Downloaded'] / 524288;
+                var Percent = (Files[Name]['Downloaded'] / Files[Name]['FileSize']) * 100;
+                socket.emit('MoreData', { 'Place' : Place, 'Percent' :  Percent});
+            });
         }
         else
         {
-            Files[Name]['Handler'] = fd; //We store the file handler so we can write to it later
-            socket.emit('MoreData', { 'Place' : Place, Percent : 0 });
-        }
-    });
-});
-socket.on('Upload', function (data){
-    var Name = data['Name'];
-    Files[Name]['Downloaded'] += data['Data'].length;
-    Files[Name]['Data'] += data['Data'];
-    if(Files[Name]['Downloaded'] == Files[Name]['FileSize']) //If File is Fully Uploaded
-    {
-        fs.write(Files[Name]['Handler'], Files[Name]['Data'], null, 'Binary', function(err, Writen){
-            //Get Thumbnail Here
-        });
-    }
-    else if(Files[Name]['Data'].length > 10485760){ //If the Data Buffer reaches 10MB
-        fs.write(Files[Name]['Handler'], Files[Name]['Data'], null, 'Binary', function(err, Writen){
-            Files[Name]['Data'] = ""; //Reset The Buffer
             var Place = Files[Name]['Downloaded'] / 524288;
             var Percent = (Files[Name]['Downloaded'] / Files[Name]['FileSize']) * 100;
             socket.emit('MoreData', { 'Place' : Place, 'Percent' :  Percent});
-        });
-    }
-    else
-    {
-        var Place = Files[Name]['Downloaded'] / 524288;
-        var Percent = (Files[Name]['Downloaded'] / Files[Name]['FileSize']) * 100;
-        socket.emit('MoreData', { 'Place' : Place, 'Percent' :  Percent});
-    }
-});
-var inp = fs.createReadStream("Temp/" + Name);
-var out = fs.createWriteStream("Video/" + Name);
-util.pump(inp, out, function(){
-    fs.unlink("Temp/" + Name, function () { //This Deletes The Temporary File
-        //Moving File Completed
+        }
     });
 });
